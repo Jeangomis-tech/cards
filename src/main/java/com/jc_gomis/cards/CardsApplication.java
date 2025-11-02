51b272ed62956716bc841896810dc9f8cc7d69b3
Jean Gomis <jeangomistech2018@gmail.com>
2025-11-02 22:47:10 +0100

    first commit

---
+++ src/main/java/com/jc_gomis/cards/CardsApplication.java
@@ -0,0 +1,13 @@
+package com.jc_gomis.cards;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CardsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CardsApplication.class, args);
+	}
+
+}
